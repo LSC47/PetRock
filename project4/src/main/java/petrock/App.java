c46ec2da5310ab9f686771b643717517f2efbc80
Lucas Chough <lsc47@pitt.edu>
2025-01-27 12:30:48 -0500

    Created maven project

---
+++ project4/src/main/java/petrock/App.java
@@ -0,0 +1,13 @@
+package petrock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
