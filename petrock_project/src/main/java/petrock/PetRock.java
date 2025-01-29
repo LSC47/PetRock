4a0b93c8a57ff6179ed16df2be12769ea5f1fdc8
abeawad123 <iba23@pitt.edu>
2025-01-29 12:36:55 -0500

    Update PetRock.java

--- petrock_project/src/main/java/petrock/PetRock.java
+++ petrock_project/src/main/java/petrock/PetRock.java
@@ -1,8 +1,31 @@
 package petrock;
 
+import java.util.Scanner;
+
+import java.util.Scanner;
 public class PetRock {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+ 
+
+   Scanner scanner = new Scanner(System.in);
+
+
+   while(true){
+   
+        System.out.println("\nMenu:");
+        System.out.println("1. Add Player");
+        System.out.println("2. Update Player Score");
+        System.out.println("3. Remove Player");
+        System.out.println("4. View Players");
+        System.out.println("q. Quit");
+        System.out.print("Enter your choice: ");
+
+        // Read user input
+       String choice = scanner.nextLine();
+
+        if (choice.equals("q")) {
+            System.out.println("Exiting program.");
+            break;  // Exit the loop and the program
+        }
     }
+
 }
